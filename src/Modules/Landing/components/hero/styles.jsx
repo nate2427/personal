635365d68a74e8c540fe0be21fa5b78dcd6ea06b
nate2427 <nlbaker@umich.edu>
2020-08-20 19:58:19 -0500

    updated the padding on the image for mobile viewports

--- src/Modules/Landing/components/hero/styles.jsx
+++ src/Modules/Landing/components/hero/styles.jsx
@@ -10,7 +10,7 @@ export const useStyles = makeStyles((theme) => ({
   heroWordsImg: {
     padding: "2rem 2rem 0 2rem",
     [theme.breakpoints.down("sm")]: {
-      padding: "2rem 1rem 0 1rem",
+      padding: "2rem 0rem 0 1rem",
     },
   },
   heroButtons: {
