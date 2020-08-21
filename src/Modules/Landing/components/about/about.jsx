d5a32b5bb230009dc0e4badacfbba08d9f71c977
nate2427 <nlbaker@umich.edu>
2020-08-21 04:54:25 -0500

    added the progress bars to the about section. Everything looks good. Moving on to the Resume section

--- src/Modules/Landing/components/about/about.jsx
+++ src/Modules/Landing/components/about/about.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, LinearProgress } from "@material-ui/core";
 
 import { useStyles } from "./styles";
 import SectionHeader from "../section-header/section-header";
@@ -21,6 +21,46 @@ export default function () {
         <Typography className={`${classes.aboutBody}`} variant="body2">
           {content.aboutBody}
         </Typography>
+      </Grid>
+      <Grid container item xs={12} className={`${classes.skillContainer}`}>
+        {content.skills.map((skill, key) => (
+          <Grid
+            key={key}
+            item
+            container
+            xs={12}
+            sm={6}
+            className={`${classes.skill}`}
+          >
+            <Grid
+              item
+              container
+              xs={12}
+              className={`${classes.skillWordContainer}`}
+            >
+              <Typography
+                className={`${classes.progressWords}`}
+                variant="h5"
+                align="left"
+              >
+                {skill.skill}
+              </Typography>
+              <Typography
+                className={`${classes.progressStatus}`}
+                variant="h5"
+                align="right"
+              >
+                {skill.status}%
+              </Typography>
+            </Grid>
+            <LinearProgress
+              color="secondary"
+              variant="determinate"
+              value={skill.status}
+              className={`${classes.progress}`}
+            />
+          </Grid>
+        ))}
       </Grid>
     </Grid>
   );
--- src/Modules/Landing/components/about/styles.jsx
+++ src/Modules/Landing/components/about/styles.jsx
@@ -10,8 +10,28 @@ export const useStyles = makeStyles((theme) => ({
     fontSize: "2rem",
     lineHeight: "4rem",
     [theme.breakpoints.down("xs")]: {
-      fontSize: "1.5rem",
+      fontSize: "1.8rem",
       lineHeight: "3rem",
     },
+  },
+  progress: {
+    width: "100%",
+    padding: ".4rem",
+  },
+  progressWords: {
+    width: "70%",
+  },
+  progressStatus: {
+    width: "30%",
+  },
+  skillContainer: {
+    paddingTop: "4rem",
+  },
+  skill: {
+    paddingRight: "2rem",
+    paddingBottom: "2rem",
+  },
+  skillWordContainer: {
+    paddingBottom: "1rem",
   },
 }));
