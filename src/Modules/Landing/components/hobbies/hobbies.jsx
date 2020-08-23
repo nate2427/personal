5f0810abc8979e94fbeb3332d90374091324f2c8
nate2427 <nlbaker@umich.edu>
2020-08-23 14:10:45 -0500

    hobbies section is complete!

--- src/Modules/Landing/components/hobbies/hobbies.jsx
+++ src/Modules/Landing/components/hobbies/hobbies.jsx
@@ -1,11 +1,42 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography, useMediaQuery, useTheme } from "@material-ui/core";
+import SportsBasketballIcon from "@material-ui/icons/SportsBasketball";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
+import RecordVoiceOverIcon from "@material-ui/icons/RecordVoiceOver";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import ExtensionIcon from "@material-ui/icons/Extension";
 
 import { useStyles } from "./styles";
 import SectionHeader from "../section-header/section-header";
 
 export default function () {
   const classes = useStyles();
+  const theme = useTheme();
+
+  const screenSize = useMediaQuery(theme.breakpoints.down("xs"));
+
+  const hobbies = [
+    {
+      icon: <ImportContactsIcon className={`${classes.icon}`} />,
+      title: "Reading",
+    },
+    {
+      icon: <ExtensionIcon className={`${classes.icon}`} />,
+      title: "Puzzles",
+    },
+    {
+      icon: <RecordVoiceOverIcon className={`${classes.icon}`} />,
+      title: "Motivational Speaking",
+    },
+    {
+      icon: <FastfoodIcon className={`${classes.icon}`} />,
+      title: "Eat",
+    },
+    {
+      icon: <SportsBasketballIcon className={`${classes.icon}`} />,
+      title: "Basketball",
+    },
+  ];
   return (
     <Grid
       container
@@ -17,6 +48,40 @@ export default function () {
       id="hobbies"
     >
       <SectionHeader title="Hobbies" description="Cheerful & Enthusiastic" />
+      <Grid container justify="center">
+        {hobbies.map((hobby, key) => {
+          return (
+            <Hobby
+              hobby={hobby}
+              size={hobbies.length - 1}
+              index={key}
+              key={key}
+            />
+          );
+        })}
+      </Grid>
     </Grid>
   );
 }
+
+const Hobby = ({ hobby, index, size }) => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      className={`${classes.hobbyContainer} ${
+        index !== size ? classes.hobbyContainer2 : ""
+      }`}
+      container
+      item
+      xs={10}
+      sm={2}
+      justify="center"
+    >
+      {hobby.icon}
+      <Typography align="center" className={`${classes.word}`} variant="h3">
+        {hobby.title}
+      </Typography>
+    </Grid>
+  );
+};
--- src/Modules/Landing/components/hobbies/styles.jsx
+++ src/Modules/Landing/components/hobbies/styles.jsx
@@ -7,4 +7,30 @@ export const useStyles = makeStyles((theme) => ({
     color: theme.palette.type === "dark" ? "#f5f5f5" : "#000",
     padding: "0 2rem",
   },
+  icon: {
+    fontSize: "10rem",
+    paddingTop: "2rem",
+    paddingBottom: "1rem",
+    color: theme.palette.type === "dark" ? "#121212" : "#f5f5f5",
+  },
+  word: {
+    width: "100%",
+    paddingBottom: "2rem",
+    color: theme.palette.type === "dark" ? "#121212" : "#f5f5f5",
+    fontSize: "2rem",
+  },
+  hobbyContainer: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  hobbyContainer2: {
+    borderRight: `${
+      theme.palette.type === "dark" ? "#000" : "#f5f5f5"
+    } solid .2rem`,
+    [theme.breakpoints.down("xs")]: {
+      borderRight: "unset",
+      borderBottom: `${
+        theme.palette.type === "dark" ? "#000" : "#f5f5f5"
+      } solid .2rem`,
+    },
+  },
 }));
